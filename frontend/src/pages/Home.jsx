270fa5326b8b88bd61b6d028565298ae0680c321
AnthonyRivera3 <Anthony.rivera01110@gmail.com>
2025-09-26 14:51:50 -0400

    Setting up the homepage format and background

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+<<<<<<< Updated upstream
 import Navbar from "../components/Navbar";
 import "./Home.css";
+=======
+import "./Home.css";
+import background from "../Photos/HP_bkg.png"; // correct relative path
+>>>>>>> Stashed changes
 
-const Home = () => {
+export default function Home() {
   return (
+<<<<<<< Updated upstream
     <div className="home-container">
       <Navbar /> {/* Navbar only appears here */}
       <div className="home-content">
@@ -12,10 +18,36 @@ const Home = () => {
         <div className="home-buttons">
           <button className="start-match-btn">Start a Match</button>
           <button className="practice-btn">Practice Problems</button>
+=======
+    <div
+      className="homepage"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+      }}
+    >
+      {/* Semi-transparent overlay */}
+      <div className="overlay" />
+
+      {/* Main content */}
+      <div className="content">
+        <h1 className="game-title">Code Royale</h1>
+        <p className="tagline">Embark on an epic adventure!</p>
+
+        <div className="buttons">
+          <button className="btn play">Play Now</button>
+          <button className="btn about">About</button>
+>>>>>>> Stashed changes
         </div>
       </div>
     </div>
   );
+<<<<<<< Updated upstream
 };
 
 export default Home;
+=======
+}
+>>>>>>> Stashed changes
